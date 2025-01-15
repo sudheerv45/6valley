4b0100dd7f56f1439e9e6eb5077c32f6e6a2c29d
sudheerv45 <sudheer.v@oneehr.net>
2025-01-15 15:26:04 +0530

    first commit

--- Models/emergencyContactModel.js
+++ Models/emergencyContactModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const emergencyContactSchema = new mongoose.Schema(
   {
     contactName: { type: String, required: true },
-    phone: { type: String, required: true, unique: true },
+    phone: { type: Number, required: true, unique: true },
     status: { type: Boolean, default: true }, // Default status is active
     isDeleted: { type: Boolean, default: false }, // Soft delete flag
   },
